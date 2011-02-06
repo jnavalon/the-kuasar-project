31c7326cc77d7f9612f6405ffa141b34a96b62eb
jnavalon@redhermes.net <jnavalon@redhermes.net@7517e53e-ff74-7ac1-6ed8-1fed07b1f63f>
2011-02-06 21:30:29 +0000

    <Minor changes>

--- trunk/kuasar/src/kuasar/gui/PluginScan.java
+++ trunk/kuasar/src/kuasar/gui/PluginScan.java
@@ -53,22 +53,22 @@ public class PluginScan {
         DefaultListModel model = new DefaultListModel();
         MainWindow.lst_Plugins.setModel(model);
         PluginInterface[] plugins = (PluginInterface[]) Configuration.getODR(Configuration.Plugins.pluginsKey);
-        if ( plugins != null) {
+        if (plugins != null) {
             for (PluginInterface plugin : plugins) {
-                try{
+                try {
                     Object data[] = new Object[3];
-                data[0] = "1";
-                data[1] = plugin.getName();
-                if(plugin.getIcon() == null)
-                    data[2] = new javax.swing.ImageIcon(getClass().getResource("/icons/plugin.png"));
-                else
-                    data[2] = plugin.getIcon();
-                model.addElement(data);
-               // plugin.Load(MainWindow, MainWindow.getClass()); //ELIMINAR DESPRES
-                }catch(Exception ex){
-                    Error.console(this.getClass().getCanonicalName() + "fillList(fun_frm_Main MainWindow) " , "Plugin " + plugin.getClass().getCanonicalName() + " poorly implemented or unsuccessfully loaded");
+                    data[0] = "1";
+                    data[1] = plugin.getName();
+                    if (plugin.getIcon() == null) {
+                        data[2] = new javax.swing.ImageIcon(getClass().getResource("/icons/plugin.png"));
+                    } else {
+                        data[2] = plugin.getIcon();
+                    }
+                    model.addElement(data.clone());
+                } catch (Exception ex) {
+                    Error.console(this.getClass().getCanonicalName() + "fillList(fun_frm_Main MainWindow) ", "Plugin " + plugin.getClass().getCanonicalName() + " poorly implemented or unsuccessfully loaded");
                 }
-                
+
             }
         }
 
