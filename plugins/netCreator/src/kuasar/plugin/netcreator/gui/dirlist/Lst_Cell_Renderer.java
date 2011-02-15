4d02e3061985b4a0a7f177d118475dfea043f153
jnavalon@redhermes.net <jnavalon@redhermes.net@7517e53e-ff74-7ac1-6ed8-1fed07b1f63f>
2011-02-15 20:46:46 +0000

    [netCreator] + Basic Navigator

---
+++ plugins/netCreator/src/kuasar/plugin/netcreator/gui/dirlist/Lst_Cell_Renderer.java
@@ -0,0 +1,40 @@
+/*
+ *  Copyright (C) 2011 jnavalon
+ * 
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ * 
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package kuasar.plugin.netcreator.gui.dirlist;
+
+import java.awt.Component;
+import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.ListCellRenderer;
+import kuasar.plugin.classMod.Panel_Opaque;
+import org.jdom.Element;
+
+/**
+ *
+ * @author jnavalon
+ */
+public class Lst_Cell_Renderer implements ListCellRenderer{
+
+    public Component getListCellRendererComponent(JList list, Object value, int index, boolean isSelected, boolean cellHasFocus) {
+        Element e = (Element)value;
+        JPanel panel;
+        panel =( isSelected ? new pn_DirListSelected(e.getAttributeValue("name"),e.getAttributeValue("type").isEmpty()): new pn_DirListUnSelected(e.getAttributeValue("name"),e.getAttributeValue("type").isEmpty()));
+        return panel;
+    }
+
+}
