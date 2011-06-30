a1054204717e8628cc9bd93382e29069dff7450f
jnavalon@redhermes.net <jnavalon@redhermes.net@7517e53e-ff74-7ac1-6ed8-1fed07b1f63f>
2011-06-30 00:56:09 +0000

    netcreator +minor changes

--- plugins/netCreator/src/kuasar/plugin/netcreator/utils/IP.java
+++ plugins/netCreator/src/kuasar/plugin/netcreator/utils/IP.java
@@ -19,8 +19,6 @@ package kuasar.plugin.netcreator.utils;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
