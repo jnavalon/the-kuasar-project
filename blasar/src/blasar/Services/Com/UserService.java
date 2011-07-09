7a16d6fde20a7117c237497a2675b75d261288d5
jnavalon@redhermes.net <jnavalon@redhermes.net@7517e53e-ff74-7ac1-6ed8-1fed07b1f63f>
2011-07-09 18:56:17 +0000

    blasar 	(dev) Service

--- blasar/src/blasar/Services/Com/UserService.java
+++ blasar/src/blasar/Services/Com/UserService.java
@@ -14,16 +14,18 @@
  *  You should have received a copy of the GNU General Public License
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
 package blasar.Services.Com;
 
 import blasar.Config;
 import blasar.Config.CMD.CHARS;
+import blasar.Info;
 import blasar.Services.Com.vms.PluginInterface;
+import blasar.Services.Com.vms.VMService;
 import blasar.Services.Exceptions.IllegalStatement;
 import blasar.Services.SocketTools;
 import java.io.IOException;
 import java.net.SocketException;
+import java.util.HashMap;
 import java.util.StringTokenizer;
 
 /**
@@ -31,38 +33,83 @@ import java.util.StringTokenizer;
  * @author Jesus Navalon i Pastor <jnavalon at redhermes dot net>
  */
 public class UserService {
+
     private SocketTools st;
-    private PluginInterface curPlugin = null;
+    VMService service = null;
 
     public UserService(SocketTools st) throws IOException, SocketException {
         boolean exit = false;
         this.st = st;
         st.setSocketTime(0);
-        st.sendLine(CHARS.INFO +"Good to see you again " + st.getUser() + "!");
-        while(!exit){
-            StringTokenizer cmd;
-            try {
-                cmd = new StringTokenizer(st.readLine(Config.CMD.ANSWER));
-                exit=splitter(cmd);
-            } catch (IllegalStatement ex) {
-                exit=true;
+        st.sendLine(CHARS.INFO + "Good to see you again " + st.getUser() + "!");
+        Info.showMessage("User " + st.getUser() + " logged successfully from " + st.getRemoteIP());
+        while (!exit) {
+            if (service == null) {
+                StringTokenizer cmd;
+                try {
+                    cmd = new StringTokenizer(st.readLine(Config.CMD.QUESTION));
+                    exit = splitter(cmd);
+                } catch (IllegalStatement ex) {
+                    exit = true;
+                }
+            } else {
+                if (service.splitter(st)) {
+                    service = null;
+                }
             }
         }
     }
 
-    private boolean splitter(StringTokenizer cmd) {
-        if(cmd==null) return false;
-        if(!cmd.hasMoreElements()) return false;
-        String act = cmd.nextToken().toLowerCase();
-        if(act.equals("exit")){
-            return true;
-        }else if(act.equals("createvm")) {
-            //createVM(cmd);
-        }else if(act.equals("listvm")){
-            
+    private boolean splitter(StringTokenizer cmd) throws SocketException, IOException {
+        if (cmd == null) {
+            return false;
         }
-            
-        return true;
+        if (!cmd.hasMoreTokens()) {
+            return false;
+        }
+
+        String act = cmd.nextToken().toLowerCase();
+        if (act.equals("exit")) {
+            return true;
+        } else if (act.equals("listvm")) {
+            sendVMs();
+        } else if (act.equals("switchvm")) {
+            switchVM(cmd);
+        }
+
+        return false;
     }
 
+    private void switchVM(StringTokenizer cmd) throws SocketException {
+        if (!cmd.hasMoreTokens()) {
+            st.sendLine(Config.CMD.CHARS.INFO + "ERROR");
+        }
+        String name = cmd.nextToken();
+        if (loadValidVM(name)) {
+            st.sendLine(Config.CMD.CHARS.INFO + "OK");
+        } else {
+            st.sendLine(Config.CMD.CHARS.INFO + "BAD");
+        }
+    }
+
+    private boolean loadValidVM(String vm) throws SocketException {
+        HashMap<String, PluginInterface> plugins = Config.BLASAR.plugins;
+        for (String key : plugins.keySet()) {
+            PluginInterface pi = plugins.get(key);
+            if (pi.getEngine().equals(vm)) {
+                service = new VMService(pi, st);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void sendVMs() throws SocketException {
+        HashMap<String, PluginInterface> plugins = Config.BLASAR.plugins;
+        st.sendLine("" + Config.CMD.CHARS.INFO + plugins.size());
+        for (String key : plugins.keySet()) {
+            PluginInterface pi = plugins.get(key);
+            st.sendLine(Config.CMD.CHARS.ANSWER + pi.getEngine());
+        }
+    }
 }
