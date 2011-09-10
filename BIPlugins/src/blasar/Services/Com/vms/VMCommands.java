d8c1923cff711819bc37228eb7fdf5cce8177085
jnavalon@redhermes.net <jnavalon@redhermes.net@7517e53e-ff74-7ac1-6ed8-1fed07b1f63f>
2011-09-10 17:43:44 +0000

    ^ More Comun Commands

--- BIPlugins/src/blasar/Services/Com/vms/VMCommands.java
+++ BIPlugins/src/blasar/Services/Com/vms/VMCommands.java
@@ -12,5 +12,10 @@ public interface VMCommands {
     public long getFreeSpace();
     public String[] getRegisteredMachines();
     public String getSysProperites(String key);
-    public boolean createvm(String name, String os);
+    public String createvm(String name, String os);
+    public String getImagePath();
+    public boolean setMemory(String uuid, Long memory);
+    public boolean storageCtl(String uuid, String type, String controller, boolean cache);
+    public boolean storageattach(String uuid, String storagectl, int port, int device, String type, String filepath, boolean passthrought);
+    public boolean addNIC(String uuid, String nicID, String type);
 }
