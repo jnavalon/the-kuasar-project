f1bf9597481175702da7b76614644057ffdd42b8
jnavalon@redhermes.net <jnavalon@redhermes.net@7517e53e-ff74-7ac1-6ed8-1fed07b1f63f>
2011-09-16 09:07:58 +0000

    + Added new commands: 	storageattach 	addNIC 	setNetwork

--- BIPlugins/src/blasar/Services/Com/vms/VMCommands.java
+++ BIPlugins/src/blasar/Services/Com/vms/VMCommands.java
@@ -11,11 +11,16 @@ package blasar.Services.Com.vms;
 public interface VMCommands {
     public long getFreeSpace();
     public String[] getRegisteredMachines();
+    public String[] getRunningMachines();
     public String getSysProperites(String key);
     public String createvm(String name, String os);
     public String getImagePath();
     public boolean setMemory(String uuid, Long memory);
     public boolean storageCtl(String uuid, String type, String controller, boolean cache);
     public boolean storageattach(String uuid, String storagectl, int port, int device, String type, String filepath, boolean passthrought);
-    public boolean addNIC(String uuid, String nicID, String type);
+    public boolean storageattach(String uuid, String storagectl, int port, int device, String type, boolean passthrought);
+    public boolean addNIC(String uuid, int nicID, String type, String mac);
+    public boolean setNetwork(String uuid, String operator, String username, 
+            String password, String mac, String ip, String mask, String gw, String dns);
+
 }
